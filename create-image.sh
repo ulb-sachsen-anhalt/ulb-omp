3a7070ecb44e5d2f8650898a86f7b5dec7df035b
Axel Bauer <axel.bauer@bibliothek.uni-halle.de>
2021-03-24 17:12:41 +0100

    wip

--- create-image.sh
+++ create-image.sh
@@ -9,4 +9,6 @@ docker build --no-cache \
     --tag "${IMAGE}" \
     -f ${DOCKER_CONF} .
 
+
+docker container stop containertest && docker container rm containertest
 docker run -dit --name containertest -p 8080:80 ${IMAGE}
