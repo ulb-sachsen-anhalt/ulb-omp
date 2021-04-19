63df13fb4e9c652b7120fe33f5fd5be7f7fb9394
Axel Bauer <axel.bauer@bibliothek.uni-halle.de>
2021-04-19 10:16:49 +0200

    initial commit

--- create-image.sh
+++ create-image.sh
@@ -2,15 +2,7 @@
 
 set -eu
 
-#DB_NAME=$1
-#DB_USER=$2
-#DB_PASS=$3
-
-#echo start with DB: $DB_NAME, $DB_USER, DB_PASS
-
 docker build -t omp-ulb .
-
-
 
 source .env
 
@@ -26,18 +18,6 @@ mkdir -pv /home/omp/volumes/config && cp ./omp.config.inc.php /home/omp/volumes/
 mkdir -pv /home/omp/volumes/files && chmod 777 /home/omp/volumes/files
 mkdir -pv /home/omp/volumes/private
 mkdir -pv /home/omp/volumes/public && chmod 777 /home/omp/volumes/public
-
-#OMP_HOME=$(find ./docker-omp -type d -name ${OMP_VERSION})
-#OMP_HOME=$OMP_HOME$PHP_TAIL
-#if [ -z "$OMP_HOME$" ] ; then echo "**OMP Version $VERSION not found!**";
-#    else echo "Version '$OMP_VERSION' found --> $OMP_HOME"; fi
-
-#echo copy $OMP_HOME/Dockerfile .
-#cp $OMP_HOME/Dockerfile .
-
-#echo copy $omp_HOME/exclude.list .
-#cp $OMP_HOME/exclude.list .
-
 
 echo try start docker-compose with docker-compose-ulb.yml
 #start omp
