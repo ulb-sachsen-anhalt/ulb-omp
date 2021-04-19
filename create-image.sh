3d2aae999d376c8a5b7c9396c2a520b3be674505
Axel Bauer <axel.bauer@bibliothek.uni-halle.de>
2021-04-19 11:08:03 +0200

    remove autocreate folders

--- create-image.sh
+++ create-image.sh
@@ -14,14 +14,14 @@ OMP_GIT=https://github.com/pkp/omp.git
 git clone ${OMP_GIT} || echo "'${OMP_GIT}' just here"
 
 
-mkdir -pv /home/omp/volumes/config
-# && chmod 777 /home/omp/volumes/config
+# please create mapped folders initially! 
+# see mapped volumes in docker-compose-omp-ulb.yml
+# ------------------------------------------------
+# --> /home/omp/volumes/config
 cp ./omp.config.inc.php /home/omp/volumes/config/
-mkdir -pv /home/omp/volumes/files
-# && chmod 777 /home/omp/volumes/files
-mkdir -pv /home/omp/volumes/private
-mkdir -pv /home/omp/volumes/public
-# && chmod 777 /home/omp/volumes/public
+# --> /home/omp/volumes/files
+# --> /home/omp/volumes/private
+# --> /home/omp/volumes/public
 
 echo try start docker-compose with docker-compose-ulb.yml
 #start omp
