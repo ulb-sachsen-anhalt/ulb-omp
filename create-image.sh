23c3ac87388e16d23d3795264d40916248678659
Axel Bauer <axel.bauer@adendorf.com>
2021-04-22 16:10:50 +0200

    cloen before build

--- create-image.sh
+++ create-image.sh
@@ -8,11 +8,11 @@ echo We are using omp  Version ${OMP_VERSION}
 PHP_TAIL=/alpine/apache/php
 OMP_GIT=https://github.com/pkp/omp.git
 
-# build image if not here
-docker build -t omp-ulb .
-
 git clone ${OMP_GIT} || echo "'${OMP_GIT}' just here"
 git --git-dir=omp/.git submodule update --init --recursive
+
+# build image if not here
+docker build -t omp-ulb .
 
 # please create mapped folders initially! 
 # see mapped volumes in docker-compose-omp-ulb.yml
@@ -29,3 +29,7 @@ echo try start docker-compose with docker-compose-ulb.yml
 docker-compose --file ./docker-compose-omp-ulb.yml down
 
 docker-compose --file ./docker-compose-omp-ulb.yml up -d
+
+# copy uni favicon
+docker cp favicon.ico omp_app_ulb:/var/www/html/favicon.ico
+
