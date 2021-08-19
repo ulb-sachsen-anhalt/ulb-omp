9e8a200b2be219f14b94dc853aef68f0245befbb
Axel Bauer <axel.bauer@bibliothek.uni-halle.de>
2021-08-19 11:34:14 +0200

    reconfigure to synchronize with ojs

--- build.sh
+++ build.sh
@@ -1,17 +1,31 @@
 #!/bin/bash
 
+set -eu 
+
+if [ $# -eq 0 ]
+  then
+    echo "Please pass: 
+            - root password for mysqldump, (e.g. *omp*)
+            - password SMTP (e.g. *arbitrary*)
+            - docker-compose project-name  (*dev* or *prod*) "
+fi
+
+DB_PASS=$1
+SMTP_PASS=$2
+TARGET=$3
+
 # most vars in .env
 source .env
 
-if  [ -z "$1" ] || ([ $1 != "dev" ] && [ $1 != "prod" ])
+if  [ -z "$3" ] || ([ $3 != "dev" ] && [ $3 != "prod" ])
     then
-        echo "Parameter 'dev' oder 'prod' fehlt"
+        echo "Third parameter 'dev' or 'prod' missing!"
         exit 0
-elif [ $1 == "dev" ]
+elif [ $3 == "dev" ]
     then
         data_dir=${PROJECT_DATA_ROOT_DEV}
         echo "We are using omp  Version: ${OMP_VERSION_ULB_PROD}"
-elif [ $1 == "prod" ]
+elif [ $3 == "prod" ]
     then
         data_dir=${PROJECT_DATA_ROOT_PROD}
         echo "We are using omp  Version: ${OMP_VERSION_ULB_DEV}"
@@ -29,29 +43,33 @@ echo "OMP data are in $data_dir/*"
 
 # place omp.config.inc.php file
 echo propagate new version of \"omp.config.inc.php\"
-cp -v ./omp.config.inc.php $data_dir/config/
 cp -v ./resources/omp.config.inc.php $data_dir/config/
-cp -v ./resources/omp$1.conf $data_dir/config/
+sed -i "s/mail_password/$SMTP_PASS/" $data_dir/config/omp.config.inc.php
+
+# place Apache configuration file for VirtualHost 
+cp -v ./resources/omp$3.conf $data_dir/config/
+
 
 # replace Host variable if in development build
-if [ $1 == "dev" ]; then
-    # cp -v ./resources/ompdev.conf $data_dir/config/
-    echo "reconfigure compose file: docker-compose-ompdev-ulb.yml"
+if [ $3 == "dev" ]; then
+    cp -v ./resources/ompdev.conf $data_dir/config/
+    echo "reconfigure config file with sed: omp.config.inc.php"
     sed -i "s/ompprod_db_ulb/ompdev_db_ulb/" $data_dir/config/omp.config.inc.php
-    cp -v ./docker-compose-ompprod-ulb.yml ./docker-compose-ompdev-ulb.yml
-    echo "sed data in docker-compose-ompdev-ulb.yml for develop server"
-    sed -i "s/ompprod/ompdev/g" ./docker-compose-ompdev-ulb.yml
-    sed -i "s/80:80/8080:80/" ./docker-compose-ompdev-ulb.yml
-    sed -i "s/443:443/8443:443/" ./docker-compose-ompdev-ulb.yml
-    sed -i "s/OMP_VERSION_ULB_PROD/OMP_VERSION_ULB_DEV/" ./docker-compose-ompdev-ulb.yml
+    echo "copy and reconfigure compose file with sed: docker-compose-ompdev.yml"
+    cp -v ./docker-compose-ompprod.yml ./docker-compose-ompdev.yml
+    echo "sed data in docker-compose-ompdev.yml for develop server"
+    sed -i "s/ompprod/ompdev/g" ./docker-compose-ompdev.yml
+    sed -i "s/80:80/8080:80/" ./docker-compose-ompdev.yml
+    sed -i "s/443:443/8443:443/" ./docker-compose-ompdev.yml
+    sed -i "s/OMP_VERSION_ULB_PROD/OMP_VERSION_ULB_DEV/" ./docker-compose-ompdev.yml
 fi
 
 echo propagate new version of \"manager.po\"
 cp -v ./locale/*.po $data_dir/config/locale/de_DE/
 
-echo try starting docker-compose with docker-compose-omp$1-ulb.yml
+echo try starting docker-compose with docker-compose-omp$3.yml
 
-./stop-omp $1
-./start-omp $1
+./stop-omp $3
+./start-omp $3
 
 
