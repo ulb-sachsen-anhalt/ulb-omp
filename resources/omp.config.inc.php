5633000033e3bde8806a3c553b58cf807b979b4d
Axel Bauer <axel.bauer@bibliothek.uni-halle.de>
2022-01-17 16:32:10 +0100

    respect new path to files

--- resources/omp.config.inc.php
+++ resources/omp.config.inc.php
@@ -209,7 +209,7 @@ connection_charset = utf8
 ; Complete path to directory to store uploaded files
 ; (This directory should not be directly web-accessible)
 ; Windows users should use forward slashes
-files_dir = files
+files_dir = /var/www/files
 
 ; Path to the directory to store public uploaded files
 ; (This directory should be web-accessible and the specified path
